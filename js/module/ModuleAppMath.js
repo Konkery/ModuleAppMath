c378550df89feb22d0c3ac623932f8cbc8caea16
Ivan Kukushkin <github-sync@outlook.com>
2023-03-01 11:53:26 +0400

    init: starting commit

---
+++ js/module/ModuleAppMath.js
@@ -0,0 +1,12 @@
+/**
+ * Метод AddIsInteger расширяет функционал объекта типа Number. Данный типа в реализации Espruino не
+ * поддерживает стандартный для JS метод isInteger.
+ */
+function AddIsInteger() {
+    /**
+     * @param {number} it  - проверяемое число
+     */
+    Number.isInteger = (it) => {return isFinite(it) && Math.floor(it) === it};
+}
+
+exports.is = AddIsInteger;
